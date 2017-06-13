8f19e5d996730006ec93f800de24ebe1cef67788
Mingquan Liu <Mingquan Liu>
2017-06-13 09:45:46 -0400

    Restart still testing

---
+++ FriendshipStudy/src/main/java/edu/cmu/chimps/friendship_study/ExceptionHandler.java
@@ -0,0 +1,8 @@
+package edu.cmu.chimps.friendship_study;
+
+/**
+ * Created by lenovo on 2017/6/11.
+ */
+
+public class ExceptionHandler {
+}
---
+++ FriendshipStudy/src/main/java/edu/cmu/chimps/friendship_study/ExceptionHandlerActivity.java
@@ -0,0 +1,8 @@
+package edu.cmu.chimps.friendship_study;
+
+/**
+ * Created by lenovo on 2017/6/12.
+ */
+
+public class ExceptionHandlerActivity {
+}
